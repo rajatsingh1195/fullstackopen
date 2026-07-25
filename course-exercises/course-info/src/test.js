3c025a854a0b928655a5d47bfb5b5d36f01abff3
Rajat Singh <rksingh3039@gmail.com>
2026-07-25 07:31:31 +0000

    completed exercise 1.1

---
+++ course-exercises/course-info/src/test.js
@@ -0,0 +1,51 @@
+/*const num = [1, 5, 7];
+const newNum = num.concat(9);
+newNum.forEach((value) => {
+  console.log(value + 3);
+});*/
+
+/*const array = [1, 2, 3, 4, 5];
+
+const newArray = array.map((value) => value * 2);
+
+console.log(newArray);*/
+
+/*const Num = [1, 2, 3, 4, 5, 6];
+const [first, second, ...rest] = Num;
+
+console.log(...rest);*/
+
+//object
+/*
+let obj = {
+  name: "Rajat",
+  gender: "male",
+  age: 30,
+};
+
+obj.city = "bhubneswar";
+
+console.log(obj);*/
+
+/*const cube = (p) => {
+  console.log(p);
+  return p * p * p;
+};
+
+const result = cube(3);
+
+console.log(result);*/
+
+/*const squre = (x) => x * x;
+const answer = squre(3);
+console.log(answer);*/
+
+/*const square = num.map((value) => value * 2);
+console.log(square);*/
+
+/*const table = [];
+
+for (let i = 1; i <= 10; i++) {
+  table.push(i * 2);
+}
+console.log(table);*/
